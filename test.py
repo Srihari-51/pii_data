d66d7841b2b848da78609cbaa91c38d10c6fbbf3
Srihari-51 <srihari@encipherhealth.com>
2024-12-26 11:40:46 +0530

    knowledgator/modern-gliner-bi-base-v1.0 test.py

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ pii_json = {
 
 # Custom SpaCy config for Gliner model
 custom_spacy_config = {
-    "gliner_model": "knowledgator/ModernBERT-large",
+    "gliner_model": "knowledgator/modern-gliner-bi-base-v1.0",
     "chunk_size": 250,
     "labels": ["sex", "gender", "person", "phone_number", "dob","age",""],
     "style": "ent",
