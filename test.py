9169fad8d0211d34e9c32beb888aa3cc2565e3ca
Srihari-51 <srihari@encipherhealth.com>
2024-12-26 11:48:34 +0530

    remove reduse

--- test.py
+++ test.py
@@ -72,8 +72,6 @@ async def extract_and_redact(input_data: TextInput):
 
     doc = nlp(input_data.text)
     texts=input_data.text
-    if len(texts)>700:
-        return "Reduse the Word Count below 700"
     matches = matcher(doc)
     extracted_entities = []
     names_gliner = []
